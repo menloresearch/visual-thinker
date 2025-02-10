1d8dceceb93b482c22e35b26030246965e236019
bachvudinh <bachvudinh02@gmail.com>
2025-02-10 05:37:52 +0000

    add config for 7B model

--- eval/inference.py
+++ eval/inference.py
@@ -6,17 +6,17 @@ model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloa
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 test_ds = load_dataset("homebrewltd/Maze-Reasoning", split="test")
 for instruction, label in zip(test_ds['Prompt'], test_ds['Response']):
-    text = f"<|start_header_id|>user<|end_header_id|>\n\n<|reserved_special_token_69|>{instruction.strip()}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
-    model_inputs = tokenizer([text], return_tensors="pt").to("cuda")
+    chat = [
+        {"role": "user", "content": instruction.strip()}
+    ]
+    tokenized_chat = tokenizer.apply_chat_template(chat, tokenize=True, add_generation_prompt=True, return_tensors="pt").to("cuda")
     generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=4096,
-    do_sample=False,
+    tokenized_chat,
+    max_new_tokens=20000,
+    do_sample=True,
     temperature=0.6,
     )
-    generated_ids = [
-        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-    output = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    output = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
     print(f"Output: {output}")
     print(f"Label: {label}")
