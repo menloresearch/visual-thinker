c6e63f82abc2d81362913aa213c0a46820fa0914
automaticcat <daogiatuank54@gmail.com>
2025-02-20 06:06:12 +0700

    Change vllm model

--- api/vllm_host.sh
+++ api/vllm_host.sh
@@ -1,4 +1,4 @@
-vllm serve jan-hq/AlphaMaze-v0.2-1.5B-GRPO-cp-600 \
+vllm serve jan-hq/AlphaMaze-v0.2-1.5B-GRPO-cp-1600 \
     --served-model-name jan-hq/AlphaMaze-v0.2-1.5B-GRPO-cp-600 \
     --host 0.0.0.0 \
     --port 3347 \
