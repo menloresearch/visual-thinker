c1e4350b6628067dd91228448871cb91ee90df5a
Nguyen Ngoc Minh <91668012+Minh141120@users.noreply.github.com>
2025-02-20 09:12:53 +0700

    chore: export model name env (#19)

--- playground/src/app/api/chat/route.ts
+++ playground/src/app/api/chat/route.ts
@@ -5,13 +5,15 @@ export async function POST(req: Request) {
   // Make a request to OpenAI's API based on
   // a placeholder prompt
   const { messages } = await req.json();
-
+  const DEFAULT_MODEL = "jan-hq/AlphaMaze-v0.2-1.5B-GRPO-cp-600";
+  const modelName = process.env.MODEL_NAME || DEFAULT_MODEL;
+  
   const openai = createOpenAI({
     baseURL: process.env.API_URL,
     apiKey: "menlo",
   });
   const response = streamText({
-    model: openai("jan-hq/AlphaMaze-v0.2-1.5B-GRPO-cp-600"),
+    model: openai(modelName),
     temperature: 0.6,
     messages,
   });
