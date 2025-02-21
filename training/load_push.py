bccf7b71ba59080af771a7e6d0b59c8b6e2bae94
bachvudinh <bachvudinh02@gmail.com>
2025-02-21 08:28:34 +0000

    delete redundant file

--- training/load_push.py
+++ training/load_push.py
@@ -2,14 +2,14 @@ from unsloth import FastLanguageModel
 max_seq_length=4096
 dtype = "bfloat16"
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "./outputs/Deepseek-Qwen2.5-7B-Redistil/checkpoint-400",
+    model_name = "./outputs/AlphaMaze-v0.2-1.5B/",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = False,
 )
-model.push_to_hub_merged("jan-hq/Deepseek-Qwen2.5-7B-Redistil-GRPO", tokenizer, save_method = "merged_16bit")
+model.push_to_hub_merged("jan-hq/AlphaMaze-v0.2-1.5B", tokenizer, save_method = "merged_16bit")
 model.push_to_hub_gguf(
-    "jan-hq/Deepseek-Qwen2.5-7B-Redistil-GRPO", # Change hf to your username!
+    "jan-hq/AlphaMaze-v0.2-1.5B", # Change hf to your username!
     tokenizer,
     quantization_method = ["q4_k_m"],
 )
